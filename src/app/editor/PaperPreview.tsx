82eb74a377d777091ae35b7c7afb175dc65791f1
T.W. Arkay <twarkay@gmail.com>
2025-10-10 16:27:22 +0000

    you removed them but the numbers are still showing in the preview

--- src/app/editor/PaperPreview.tsx
+++ src/app/editor/PaperPreview.tsx
@@ -81,7 +81,6 @@ const renderQuestionPreview = (question: Question, index: number) => {
               <div key={sq.id}>
                 <div className="flex justify-between">
                   <p>{getNumbering(question.numberingFormat, sqIndex)}) {sq.content}</p>
-                  <p>{sq.marks || ''}</p>
                 </div>
                 {sq.options && sq.options.length > 0 && (
                   <div className="pl-6 mt-2 grid grid-cols-2 gap-x-8 gap-y-2">
@@ -131,7 +130,3 @@ export default function PaperPreview({ paper }: { paper: Paper }) {
     </div>
   );
 }
-
-    
-
-    
