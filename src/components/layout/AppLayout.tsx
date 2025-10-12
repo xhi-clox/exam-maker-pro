8d8c5ca0894e60ffd2b79cee7962c4e704b4bb3e
T.W. Arkay <twarkay@gmail.com>
2025-10-12 11:19:58 +0000

    remove this header (_for element <AppHeader>_)

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,10 +1,7 @@
 
-import { AppHeader } from './AppHeader';
-
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col bg-muted/40">
-      <AppHeader />
       <main className="flex-1">{children}</main>
     </div>
   );
