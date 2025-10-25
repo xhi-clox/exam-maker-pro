c45d4e4ff6855712c243a8e9c80888ec78f964cb
T.W. Arkay <twarkay@gmail.com>
2025-10-25 16:47:30 +0000

    still a lots of space at the bottom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <div className="flex min-h-screen w-full flex-col bg-muted/40">
           <AppHeader />
-          <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+          <main className="flex flex-1 flex-col">
               {children}
           </main>
         </div>
